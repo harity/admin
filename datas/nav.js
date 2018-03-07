58b6ed8951ac9cd0fdce851d6ba1af1f7434aea4
zsy <985463770@qq.com>
2018-03-07 13:11:27 +0800

    layui 后台布局

---
+++ datas/nav.js
@@ -0,0 +1,53 @@
+var navs = [{
+	"title": "权限管理",
+	"icon": "&#xe61d;",
+	"spread": true,
+	"children": [{
+		"title": "用户管理",
+		"icon": "&#xe613;",
+		"href": "UserManagement.html"
+	}, {
+		"title": "权限管理",
+		"icon": "&#xe61d;",
+		"href": "AuthorityManagement.html"
+	}]
+}, {
+	"title": "用户管理",
+	"icon": "&#xe612;",
+	"spread": false,
+	"children": [{
+		"title": "玩家用户管理",
+		"icon": "&#xe612;",
+		"href": "UserPlayerManagemen.html"
+	}]
+}, {
+	"title": "操作管理",
+	"icon": "&#xe614;",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "提现审核",
+		"icon": "&#xe65e;",
+		"href": "Withdraw.html"
+	}, {
+		"title": "发布试玩",
+		"icon": "&#xe60c;",
+		"href": "ReleaseDemo.html"
+	}, {
+		"title": "发布信息",
+		"icon": "&#xe63a;",
+		"href": "ReleaseMsg.html"
+	}, {
+		"title": "发布攻略",
+		"icon": "&#xe6c6;",
+		"href": "Strategy.html"
+	}, {
+		"title": "试玩推荐",
+		"icon": "&#xe60e;",
+		"href": "DemoRecommend.html"
+	}, {
+		"title": "专链管理",
+		"icon": "&#xe64c;",
+		"href": "Link.html"
+	}]
+}];
